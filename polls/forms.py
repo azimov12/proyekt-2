a3975bf082a145fea0eede062ed9a8d6d329ae73
habibullo <azimovhabibullo1@gmail.com>
2023-09-24 22:22:48 +0500

    translation-commit 2

--- polls/forms.py
+++ polls/forms.py
@@ -2,15 +2,15 @@ from django import forms
 from .models import TodoModel
 
 
-class TodoForm(forms.ModelForm):
-    task_name_uz = forms.CharField()
-    task_name_en = forms.CharField()
-    task_name_ru = forms.CharField()
+class NoutbookForm(forms.ModelForm):
+    noutbook_name_uz = forms.CharField()
+    noutbook_name_en = forms.CharField()
+    noutbook_name_ru = forms.CharField()
 
-    description_uz = forms.CharField()
-    description_en = forms.CharField()
-    description_ru = forms.CharField()
+    information_uz = forms.CharField()
+    information_en = forms.CharField()
+    information_ru = forms.CharField()
 
     class Meta:
         model = TodoModel
-        exclude = ('task_name', 'description')
+        exclude = ('noutbook_name', 'information')
